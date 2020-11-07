bba2ec261147ee534fbc23f54765f36ab486b947
viewtifult <tashanduncan1994@gmail.com>
2020-11-07 12:46:43 +0000

    updated quiz complete

--- js/quiz.js
+++ js/quiz.js
@@ -3,14 +3,14 @@ const questions = [
     ['What year was Tashan born', '1994'], 
     ['what computer game does Tashan play the most currently', 'overwatch'], 
     ['what programming language is this quiz written in', 'javascript']
-  ]
+  ];
 
 // 2. Store the number of questions answered correctly
-let correct = 0
+let correct = 0;
 
 // Set arrays for correct and incorrect questions that the user has answered
-const rightQuestions = []
-const wrongQuestions = []
+const rightQuestions = [];
+const wrongQuestions = [];
 /* 
   3. Use a loop to cycle through each question
       - Present each question to the user
@@ -23,17 +23,30 @@ for (let i = 0; i < questions.length; i++) {
   const answer = questions[i][1];
   const userAnswer = prompt(question);
   if (userAnswer.toLowerCase() === answer){
-    rightQuestions.push(question)
-    correct++
+    rightQuestions.push(question);
+    correct++;
   } else {
-    alert('Incorrect')
-    wrongQuestions.push(question)
+    alert('Incorrect');
+    wrongQuestions.push(question);
   }
 }
 
+// Loop through Array, create list item which stores each value in a li tag
+const createList = (arr) => {
+  const newList = [];
+  for(let i = 0; i < arr.length; i++) {
+    newList.push(`<li>${arr[i]}</li>`);
+  }
+  return newList.join("");
+}
+
 // 4. Display the number of correct answers to the user
+const result = `<h1>Correct Answers = ${correct} of out ${questions.length}</h1>`;
+const rightList = createList(rightQuestions);
+const wrongList = createList(wrongQuestions);
 
-const result = `<h1>Correct Answers = ${correct} of out ${questions.length}</h1>`
-
-
-document.querySelector('main').innerHTML = `${result}` + `<h2>You got these questions right:</h2>`
+document.querySelector('main').innerHTML = `${result}` + 
+  `<h2>You got these questions right:</h2>` + 
+  `<ol>${rightList}</ol>` +
+  `<h2>You got these questions wrong:</h2>` + 
+  `<ol>${wrongList}</ol>`;
