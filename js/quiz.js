f828b381c6f7b6aa4af0342e5da8c6688b26742e
viewtifult <tashanduncan1994@gmail.com>
2020-11-07 10:41:52 +0000

    create questions and answers and store correct answers in a var

---
+++ js/quiz.js
@@ -0,0 +1,15 @@
+// 1. Create a multidimensional array to hold quiz questions and answers
+const questions = [['What year was Tashan born', 1994], ['what computer game does Tashan play the most currently', 'Overwatch'], ['what programming language is this quiz written in', 'JavaScript']]
+
+// 2. Store the number of questions answered correctly
+let correct
+
+/* 
+  3. Use a loop to cycle through each question
+      - Present each question to the user
+      - Compare the user's response to answer in the array
+      - If the response matches the answer, the number of correctly
+        answered questions increments by 1
+*/
+
+// 4. Display the number of correct answers to the user
