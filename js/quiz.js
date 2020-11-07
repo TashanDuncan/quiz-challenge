00363958070fa98df588ede38095da8208483533
viewtifult <tashanduncan1994@gmail.com>
2020-11-07 11:34:46 +0000

    Expanded on quiz, added new arrays for the questions which were answered right and wrong respectively

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,16 @@
 // 1. Create a multidimensional array to hold quiz questions and answers
-const questions = [['What year was Tashan born', '1994'], ['what computer game does Tashan play the most currently', 'overwatch'], ['what programming language is this quiz written in', 'javascript']]
+const questions = [
+    ['What year was Tashan born', '1994'], 
+    ['what computer game does Tashan play the most currently', 'overwatch'], 
+    ['what programming language is this quiz written in', 'javascript']
+  ]
 
 // 2. Store the number of questions answered correctly
 let correct = 0
 
-
+// Set arrays for correct and incorrect questions that the user has answered
+const rightQuestions = []
+const wrongQuestions = []
 /* 
   3. Use a loop to cycle through each question
       - Present each question to the user
@@ -13,13 +19,21 @@ let correct = 0
         answered questions increments by 1
 */
 for (let i = 0; i < questions.length; i++) {
-  let answer = prompt(`${questions[i][0]}`);
-  if (answer.toLowerCase() === questions[i][1]){
+  const question = questions[i][0];
+  const answer = questions[i][1];
+  const userAnswer = prompt(question);
+  if (userAnswer.toLowerCase() === answer){
+    rightQuestions.push(question)
     correct++
   } else {
     alert('Incorrect')
+    wrongQuestions.push(question)
   }
 }
 
 // 4. Display the number of correct answers to the user
-document.querySelector('main').innerHTML = `<h1>Correct Answers = ${correct} of out ${questions.length}</h1>`
+
+const result = `<h1>Correct Answers = ${correct} of out ${questions.length}</h1>`
+
+
+document.querySelector('main').innerHTML = `${result}` + `<h2>You got these questions right:</h2>`
